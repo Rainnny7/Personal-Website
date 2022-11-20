d21a6a358cfc744a993a4943d9c44b4e32d8cdbb
Braydon <xcoolmanxx@gmail.com>
2022-11-20 02:39:40 -0500

    Properly layout the home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,15 @@
-export default function Home() {
+import Discord from "../components/discord";
+import Hero from "../components/hero";
+import Intro from "../components/intro";
+
+export default async function Home() {
 	return (
-		<div className="bg-black">
-			<a className="text-blue-400">Hello World</a>
+		<div className="flex flex-col flex-wrap justify-center mt-48 sm:mt-56">
+			<Hero />
+			<div className="flex flex-col items-center gap-10 p-10 mt-48 sm:items-start justify-evenly sm:flex-row">
+				<Discord />
+				<Intro />
+			</div>
 		</div>
 	);
 }

da9033bc67b5e21b451345db76f3a7288418faae
Braydon <xcoolmanxx@gmail.com>
2022-11-20 06:53:05 -0500

    Make the button primary option actually work

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -39,14 +39,22 @@ export default function Hero() {
 					return (
 						<Link key={index} href={button.link}>
 							<ScaleUpHoverAnimation>
-								<button className="flex gap-2 px-5 py-2 text-lg rounded-full text-slate-400 bg-slate-900">
-									<Image
-										src={button.icon}
-										alt={button.text + " Button"}
-										width={28}
-										height={28}
-										unoptimized
-									/>
+								<button
+									className={`flex gap-2 px-5 py-2 text-lg rounded-full text-slate-400 ${
+										button.primary
+											? "bg-slate-900"
+											: "border-2 border-slate-700"
+									}`}
+								>
+									{button.icon && (
+										<Image
+											src={button.icon}
+											alt={button.text + " Button"}
+											width={28}
+											height={28}
+											unoptimized
+										/>
+									)}
 									<span>{button.text}</span>
 								</button>
 							</ScaleUpHoverAnimation>

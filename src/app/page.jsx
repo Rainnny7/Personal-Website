39646bf5260c59afda4630e5ee43b1d2ae670f6f
Braydon <xcoolmanxx@gmail.com>
2022-11-20 06:10:24 -0500

    Responsive!

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,7 +6,7 @@ export default async function Home() {
 	return (
 		<div className="flex flex-col flex-wrap justify-center mt-48 sm:mt-56">
 			<Hero />
-			<div className="flex flex-col items-center gap-10 p-10 mt-48 sm:items-start justify-evenly sm:flex-row">
+			<div className="flex flex-col items-center gap-24 p-10 mt-48 sm:gap-4 sm:items-start justify-evenly sm:flex-row">
 				<Discord />
 				<Intro />
 			</div>

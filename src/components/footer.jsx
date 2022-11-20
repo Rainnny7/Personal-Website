c8c7061c6be34ac822b2ce3b66e8aa6ef3d1cc21
Braydon <xcoolmanxx@gmail.com>
2022-11-19 19:03:23 -0500

    Added links to the footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -11,8 +11,10 @@ export default function Footer() {
 	const date = new Date();
 	return (
 		<div className="absolute inset-x-0 bottom-0 flex justify-between px-6 pb-5 font-serif opacity-80 text-slate-500">
-			<a>rainnny.club ©️ {date.getFullYear()}</a>
-			<a>Made with ❤️ by Rainnny</a>
+			<a href="https://rainnny.club">rainnny.club ©️ {date.getFullYear()}</a>
+			<a href="https://github.com/Rainnny7/personal-website">
+				Made with ❤️ by Rainnny
+			</a>
 		</div>
 	);
 }

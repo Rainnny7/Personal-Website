0ccc386c39c7cb6e2c434ca154fbe5884ef294bc
Braydon <xcoolmanxx@gmail.com>
2022-11-20 04:54:02 -0500

    Added a fade up animation

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,15 +1,20 @@
+import "react-loading-skeleton/dist/skeleton.css";
 import Footer from "../components/footer";
 import Navbar from "../components/navbar";
+import FadeUpAnimation from "../util/fadeUpAnimation";
 import "./globals.css";
+import { AppProvider } from "./provider";
 
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en" dir="auto">
 			<head />
 			<body>
-				<Navbar />
-				{children}
-				<Footer />
+				<AppProvider>
+					<Navbar />
+					<FadeUpAnimation>{children}</FadeUpAnimation>
+					<Footer />
+				</AppProvider>
 			</body>
 		</html>
 	);
---
+++ src/util/fadeUpAnimation.jsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { motion } from "framer-motion";
+
+/**
+ * @summary A fade up animation.
+ * @description This children of this animation will slowly fade up and appear.
+ * @param {*} children The children to animate.
+ */
+export default function FadeUpAnimation({ children }) {
+	return (
+		<motion.div
+			key="modal"
+			initial={{
+				opacity: 0,
+				y: 50,
+			}}
+			animate={{
+				opacity: 1,
+				y: 0,
+				transition: {
+					y: {
+						duration: 0.7,
+					},
+				},
+			}}
+		>
+			{children}
+		</motion.div>
+	);
+}

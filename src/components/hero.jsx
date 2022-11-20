5c3533426173cf1992337e37efca8d219433b7ed
Braydon <xcoolmanxx@gmail.com>
2022-11-20 02:54:58 -0500

    Make the hero component more responsive

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -13,10 +13,10 @@ export default function Hero() {
 	return (
 		<div className="flex flex-col gap-3 px-6 text-center">
 			{/* Name */}
-			<a className="text-5xl font-bold text-slate-300">Braydon Rainnny</a>
+			<a className="text-4xl font-bold text-slate-300">Braydon Rainnny</a>
 
 			{/* Typewriter */}
-			<a className="flex flex-wrap justify-center font-serif text-3xl text-center text-slate-400">
+			<a className="flex flex-wrap justify-center font-serif text-xl text-center text-slate-400 md:text-3xl">
 				<span className="mr-2">I am a</span>
 				<Typewriter
 					onInit={(typewriter) => {
@@ -32,7 +32,7 @@ export default function Hero() {
 			</a>
 
 			{/* Buttons */}
-			<div className="flex justify-center gap-4 mt-6">
+			<div className="flex justify-center gap-4 mt-12">
 				{/* My Work */}
 				<Link href="#myWork">
 					<button className="flex gap-1 px-5 py-2 text-lg transition duration-300 ease-in-out delay-150 rounded-full bg-slate-400 hover:-translate-y-1 hover:scale-95">

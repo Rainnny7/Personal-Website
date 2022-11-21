ee83121d27f7f99ddbc58440d28e3d944a2b0966
Braydon <xcoolmanxx@gmail.com>
2022-11-21 17:41:10 -0500

    Hero component spacing

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { ArrowSmallDownIcon } from "@heroicons/react/24/outline";
 import Image from "next/image";
 import React from "react";
 import Typewriter from "typewriter-effect";
@@ -15,56 +16,64 @@ import ScaleUpHoverAnimation from "../util/animation/scaleUpHoverAnimation";
 export default function Hero() {
 	return (
 		<FadeUpAnimation y={70} animation={1.4}>
-			<div className="flex flex-col items-center justify-center h-screen gap-3 px-6 text-center -mt-14">
-				{/* Name */}
-				<a className="text-5xl font-bold text-slate-300">
-					{config.hero.header}
-				</a>
+			<div className="flex flex-col items-center justify-between h-screen gap-3 px-6 text-center -mt-14">
+				{/* Top */}
+				<div className="mt-[35vh] sm:mt-[40vh]">
+					{/* Name */}
+					<a className="text-5xl font-bold text-slate-300">
+						{config.hero.header}
+					</a>
 
-				{/* Typewriter */}
-				<a className="flex flex-wrap justify-center text-xl text-center text-slate-400 md:text-3xl">
-					<span>I am a &#8203;</span>
-					<Typewriter
-						onInit={(typewriter) => {
-							typewriter.pauseFor(2500).deleteAll().start();
-						}}
-						options={{
-							strings: config.hero.text,
-							autoStart: true,
-							loop: true,
-							delay: 110,
-						}}
-					/>
-				</a>
+					{/* Typewriter */}
+					<a className="flex flex-wrap justify-center text-xl text-center text-slate-400 md:text-3xl">
+						<span>I am a &#8203;</span>
+						<Typewriter
+							onInit={(typewriter) => {
+								typewriter.pauseFor(2500).deleteAll().start();
+							}}
+							options={{
+								strings: config.hero.text,
+								autoStart: true,
+								loop: true,
+								delay: 110,
+							}}
+						/>
+					</a>
 
-				{/* Buttons */}
-				<div className="flex flex-wrap justify-center gap-4 mt-12">
-					{config.hero.buttons.map((button, index) => {
-						return (
-							<a key={index} href={button.link}>
-								<ScaleUpHoverAnimation>
-									<button
-										className={`flex gap-2 px-5 py-2 text-lg rounded-full text-slate-400 ${
-											button.primary
-												? "bg-slate-900"
-												: "h-11 border border-slate-700"
-										}`}
-									>
-										{button.icon && (
-											<Image
-												src={button.icon}
-												alt={button.text + " Button"}
-												width={28}
-												height={28}
-												unoptimized
-											/>
-										)}
-										<span>{button.text}</span>
-									</button>
-								</ScaleUpHoverAnimation>
-							</a>
-						);
-					})}
+					{/* Buttons */}
+					<div className="flex flex-wrap justify-center gap-4 mt-12">
+						{config.hero.buttons.map((button, index) => {
+							return (
+								<a key={index} href={button.link}>
+									<ScaleUpHoverAnimation>
+										<button
+											className={`flex gap-2 px-5 py-2 text-lg rounded-full text-slate-400 ${
+												button.primary
+													? "bg-slate-900"
+													: "h-11 border border-slate-700"
+											}`}
+										>
+											{button.icon && (
+												<Image
+													src={button.icon}
+													alt={button.text + " Button"}
+													width={28}
+													height={28}
+													unoptimized
+												/>
+											)}
+											<span>{button.text}</span>
+										</button>
+									</ScaleUpHoverAnimation>
+								</a>
+							);
+						})}
+					</div>
+				</div>
+
+				{/* Bouncing Arrow */}
+				<div className="mb-14 animate-bounce">
+					<ArrowSmallDownIcon width={32} fill="white" />
 				</div>
 			</div>
 		</FadeUpAnimation>

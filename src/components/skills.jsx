b0a3ad31c277e7278a9971dce7206010b051f288
Braydon <xcoolmanxx@gmail.com>
2022-11-21 03:31:27 -0500

    Added the skills component

---
+++ src/components/skills.jsx
@@ -0,0 +1,45 @@
+import { WrenchScrewdriverIcon } from "@heroicons/react/24/outline";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import config from "../config";
+import Card, { CardIcon } from "./card";
+
+export default function Skills() {
+	return (
+		<div className="w-full">
+			<Card className="px-4 pb-8 mx-auto cursor-default">
+				{/* Icon */}
+				<CardIcon className="w-20 h-20 mx-auto my-7">
+					<WrenchScrewdriverIcon
+						className="text-white"
+						width={72}
+						height={72}
+					/>
+				</CardIcon>
+
+				{/* Header */}
+				<h1 className="mt-3 font-serif text-3xl font-bold text-white">
+					Skills
+				</h1>
+
+				{/* Skills */}
+				<div className="flex flex-wrap justify-center max-w-2xl gap-3 mt-7">
+					{config.skills.map((skill, index) => {
+						return (
+							<Link key={index} href={skill.link}>
+								<Image
+									src={skill.icon}
+									alt={skill.name}
+									width={54}
+									height={54}
+									unoptimized
+								/>
+							</Link>
+						);
+					})}
+				</div>
+			</Card>
+		</div>
+	);
+}
--- src/config.js
+++ src/config.js
@@ -57,6 +57,117 @@ const config = {
 		),
 		image: "/me.jpg",
 	},
+	skills: [
+		// Languages
+		{
+			name: "Java",
+			icon: "https://img.icons8.com/color/2x/java-coffee-cup-logo.png",
+			link: "https://www.java.com",
+		},
+		{
+			name: "JavaScript",
+			icon: "https://img.icons8.com/fluent/2x/javascript.png",
+			link: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
+		},
+		{
+			name: "CSS",
+			icon: "https://img.icons8.com/fluent/2x/css3.png",
+			link: "https://www.w3schools.com/css",
+		},
+
+		// Operating Systems
+		{
+			name: "Linux",
+			icon: "https://img.icons8.com/color/2x/linux.png",
+			link: "https://www.linux.org",
+		},
+		{
+			name: "Bash",
+			icon: "https://img.icons8.com/color/2x/bash.png",
+			link: "https://www.gnu.org/software/bash",
+		},
+
+		// Databases
+		{
+			name: "MariaDB",
+			icon: "https://img.icons8.com/fluent/2x/maria-db.png",
+			link: "https://mariadb.org",
+		},
+		{
+			name: "MongoDB",
+			icon: "https://img.icons8.com/color/2x/mongodb.png",
+			link: "https://www.mongodb.com",
+		},
+		{
+			name: "Redis",
+			icon: "https://img.icons8.com/color/2x/redis.png",
+			link: "https://redis.io",
+		},
+
+		// Software
+		{
+			name: "Git",
+			icon: "https://img.icons8.com/color/2x/git.png",
+			link: "https://git-scm.com",
+		},
+		{
+			name: "Docker",
+			icon: "https://img.icons8.com/fluent/2x/docker.png",
+			link: "https://www.docker.com",
+		},
+		{
+			name: "Jenkins",
+			icon: "https://img.icons8.com/color/2x/jenkins.png",
+			link: "https://www.jenkins.io",
+		},
+		{
+			name: "Figma",
+			icon: "https://img.icons8.com/fluent/2x/figma.png",
+			link: "https://www.figma.com",
+		},
+		{
+			name: "Postman",
+			icon: "https://img.icons8.com/dusk/2x/postman-api.png",
+			link: "https://www.postman.com",
+		},
+
+		// Frameworks & Libraries
+		{
+			name: "Maven",
+			icon: "/maven.png",
+			link: "https://maven.apache.org",
+		},
+		{
+			name: "NPM",
+			icon: "https://img.icons8.com/color/2x/npm.png",
+			link: "https://www.npmjs.com",
+		},
+		{
+			name: "React",
+			icon: "https://img.icons8.com/dusk/2x/react.png",
+			link: "https://reactjs.org/",
+		},
+		{
+			name: "NextJS",
+			icon: "https://img.icons8.com/color/2x/nextjs.png",
+			link: "https://nextjs.org/",
+		},
+		{
+			name: "TailwindCSS",
+			icon: "https://img.icons8.com/color/2x/tailwindcss.png",
+			link: "https://tailwindcss.com",
+		},
+		{
+			name: "Redux",
+			icon: "https://img.icons8.com/color/2x/redux.png",
+			link: "https://redux.js.org",
+		},
+		{
+			name: "Nginx",
+			icon: "https://img.icons8.com/color/2x/nginx.png",
+			link: "https://www.nginx.com",
+		},
+	],
 	footer: [
 		{
 			text: "rainnny.club ©️ $year",

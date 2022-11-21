638d27e67fa3e1ee56f07605daa75001d7abd6e2
Braydon <xcoolmanxx@gmail.com>
2022-11-21 02:50:17 -0500

    forgot this

--- src/components/work.jsx
+++ src/components/work.jsx
@@ -11,7 +11,7 @@ export default function Work() {
 	return (
 		<div id="work" className="w-full">
 			<Card className="px-4 pb-8 mx-auto cursor-default">
-				<CardIcon className="w-20 h-20 mx-auto">
+				<CardIcon className="w-20 h-20 mx-auto my-7">
 					<BriefcaseIcon className="text-white" width={72} height={72} />
 				</CardIcon>
 				<h1 className="mt-3 font-serif text-3xl font-bold text-white">

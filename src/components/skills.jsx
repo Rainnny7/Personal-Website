1d0b487ff5447e8e5e01a9ef1f44af1f9f0e9158
Braydon <xcoolmanxx@gmail.com>
2022-11-21 18:26:06 -0500

    Slight change to the skills component

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -1,24 +1,25 @@
-import { ClipboardIcon } from "@heroicons/react/24/outline";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import config from "../config";
 import FadeUpAnimation from "../util/animation/fadeUpAnimation";
-import Card, { CardIcon } from "./card";
+import Card from "./card";
 
 export default function Skills() {
 	return (
 		<FadeUpAnimation>
 			<div id="skills" className="w-full">
+				{/* Header */}
+				<div className="flex flex-col items-center text-center mb-7">
+					<h1 className="font-mono text-4xl font-bold text-white">Skills</h1>
+					<h3 className="max-w-md text-xl text-slate-300/60">
+						Here is a list of the skills I have aquired over my time as a
+						software engineer.
+					</h3>
+				</div>
+
+				{/* Skills */}
 				<Card className="px-4 pb-8 mx-auto cursor-default">
-					{/* Icon */}
-					<CardIcon className="w-20 h-20 mx-auto my-7">
-						<ClipboardIcon className="text-white" width={72} height={72} />
-					</CardIcon>
-
-					{/* Header */}
-					<h1 className="mt-3 text-3xl font-bold text-white">Skills</h1>
-
 					{/* Skills */}
 					<div className="flex flex-wrap justify-center max-w-2xl gap-3 mt-7">
 						{config.skills.map((skill, index) => {

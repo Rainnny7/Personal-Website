f0ba83463ac5fec82f1c06fbb03f523659b57de0
Braydon <xcoolmanxx@gmail.com>
2022-11-21 02:49:54 -0500

    Fix spacing on the home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,6 +2,7 @@ import BackgroundPattern from "../components/background";
 import Discord from "../components/discord";
 import Hero from "../components/hero";
 import Intro from "../components/intro";
+import Skills from "../components/skills";
 import Work from "../components/work";
 
 /**
@@ -14,12 +15,15 @@ export default async function Home() {
 			<BackgroundPattern />
 			<div className="relative flex flex-col flex-wrap justify-center mt-48 sm:mt-56">
 				<Hero />
-				<div className="flex flex-col items-center gap-24 p-10 mt-48 sm:gap-4 sm:items-start justify-evenly sm:flex-row">
-					<Discord />
-					<Intro />
-				</div>
-				<div className="w-full max-w-5xl p-10 mx-auto mt-20 sm:mt-52">
-					<Work />
+				<div className="flex flex-col mt-48">
+					<div className="flex flex-col items-center gap-24 p-10 sm:gap-4 sm:items-start justify-evenly sm:flex-row">
+						<Discord />
+						<Intro />
+					</div>
+					<div className="flex flex-col w-full max-w-5xl p-10 mx-auto mt-48 gap-72">
+						<Skills />
+						<Work />
+					</div>
 				</div>
 			</div>
 		</>

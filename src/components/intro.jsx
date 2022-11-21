91068a286fffbde90bd880ae6e0d782d80974c15
Braydon <xcoolmanxx@gmail.com>
2022-11-21 06:51:50 -0500

    Animate the intro component

--- src/components/intro.jsx
+++ src/components/intro.jsx
@@ -1,5 +1,8 @@
+"use client";
+
 import Image from "next/image";
 import React from "react";
+import Tilt from "react-parallax-tilt";
 import config from "../config";
 import Card, { CardIcon } from "./card";
 
@@ -10,39 +13,50 @@ import Card, { CardIcon } from "./card";
  */
 export default function Intro() {
 	return (
-		<div className="w-full max-w-[37rem]">
-			<Card className="px-4 pb-3 mx-auto cursor-default">
-				{/* Icon */}
-				<CardIcon className="w-20 h-20 mx-auto my-7">
-					<Image
-						className="mx-auto"
-						src="/hand-wave.png"
-						alt="Waving Hand"
-						width={72}
-						height={72}
-						unoptimized
-					/>
-				</CardIcon>
+		<Tilt
+			className="ml-auto mr-auto md:ml-0 md:mr-0"
+			perspective={2000}
+			tiltMaxAngleX={7}
+			tiltMaxAngleY={7}
+			gyroscope
+			tiltReverse
+		>
+			<div className="w-full max-w-[37rem]">
+				<Card className="px-4 pb-3 mx-auto cursor-default">
+					{/* Icon */}
+					<CardIcon className="w-20 h-20 mx-auto my-7">
+						<Image
+							className="mx-auto"
+							src="/hand-wave.png"
+							alt="Waving Hand"
+							width={72}
+							height={72}
+							unoptimized
+						/>
+					</CardIcon>
 
-				{/* Name & Bio */}
-				<div className="flex flex-col gap-4 mt-3">
-					<h1 className="font-serif text-3xl font-bold text-white">
-						{config.intro.header}
-					</h1>
-					<div className="flex flex-col items-center gap-4 lg:flex-row lg:gap-0 justify-evenly">
-						<p className="max-w-md text-lg text-white/50">{config.intro.bio}</p>
-						<div className="relative w-24 h-24 rounded-full">
-							<Image
-								className="object-cover rounded-full shadow-md opacity-90 shadow-white/70"
-								src={config.intro.image}
-								alt="Picture of the site owner"
-								unoptimized
-								fill
-							/>
+					{/* Name & Bio */}
+					<div className="flex flex-col gap-4 mt-3">
+						<h1 className="font-serif text-3xl font-bold text-white">
+							{config.intro.header}
+						</h1>
+						<div className="flex flex-col items-center gap-4 lg:flex-row lg:gap-0 justify-evenly">
+							<p className="max-w-md text-lg text-white/50">
+								{config.intro.bio}
+							</p>
+							<div className="relative w-24 h-24 rounded-full">
+								<Image
+									className="object-cover rounded-full shadow-md opacity-90 shadow-white/70"
+									src={config.intro.image}
+									alt="Picture of the site owner"
+									unoptimized
+									fill
+								/>
+							</div>
 						</div>
 					</div>
-				</div>
-			</Card>
-		</div>
+				</Card>
+			</div>
+		</Tilt>
 	);
 }
